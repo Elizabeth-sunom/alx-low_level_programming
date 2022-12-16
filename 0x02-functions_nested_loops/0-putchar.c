e06374642704e0dade0c441bac6aa342ee3bf18a
elizabeth <sunomafriquee@gmail.com>
2022-12-16 05:59:42 -0800

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - print _putchar
+ * iswritten by bwave
+ * Return: 0
+ */
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('p');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+
+return (id);
+}
